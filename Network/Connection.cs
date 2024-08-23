01761cccda818337b0a0d3aab9626399e0bc4b9c
Sanito Gonzalez <ddalkisoup23@gmail.com>
2024-08-23 14:09:14 +0900

    Add schemas and configurable compile script

--- Network/Connection.cs
+++ Network/Connection.cs
@@ -1,15 +1,115 @@
 using Godot;
+using Google.FlatBuffers;
 using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Tower.Network;
 
 public partial class Connection : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    private readonly TcpClient _client = new TcpClient();
+    private NetworkStream _stream;
+    private Task _io;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public async Task ConnectAsync(string host, int port)
+    {
+        GD.Print($"[{nameof(Connection)}] Connecting to {host}:{port}...");
+        try
+        {
+            await _client.ConnectAsync(host, port);
+            _stream = _client.GetStream();
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            GD.PrintErr($"[{nameof(Connection)}] port out of range: {port}");
+            return;
+        }
+        catch (Exception ex)
+        {
+            GD.PrintErr($"[{nameof(Connection)}] Error connecting");
+            return;
+        }
+    }
+
+    public void Disconnect()
+    {
+        GD.Print($"[{nameof(Connection)}] Disconnecting...");
+        _stream?.Close();
+        _client?.Close();
+    }
+
+    public void Run()
+    {
+        _ = Task.Run(async () =>
+        {
+            while (_client.Connected)
+            {
+                byte[] buffer = await ReceivePacketAsync();
+                await HandlePacketAsync(new ByteBuffer(buffer));
+            }
+        });
+    }
+    
+    public override void _ExitTree()
+    {
+        Disconnect();
+        base._ExitTree();
+    }
+
+    private async Task<byte[]> ReceivePacketAsync()
+    {
+        if (!_client.Connected) return [];
+
+        try
+        {
+            var headerBuffer = new byte[FlatBufferConstants.SizePrefixLength];
+            await _stream.ReadExactlyAsync(headerBuffer, 0, headerBuffer.Length);
+
+
+            var bodyBuffer = new byte[ByteBufferUtil.GetSizePrefix(new ByteBuffer(headerBuffer))];
+            await _stream.ReadExactlyAsync(bodyBuffer, 0, bodyBuffer.Length);
+
+            return bodyBuffer;
+        }
+        catch (OperationCanceledException)
+        {
+        }
+        catch (Exception ex)
+        {
+            GD.PrintErr($"[{nameof(Connection)}] Error reading: {ex}");
+            Disconnect();
+        }
+
+        return [];
+    }
+
+    private async Task SendPacketAsync(ByteBuffer buffer)
+    {
+        if (!_client.Connected) return;
+
+        try
+        {
+            await _stream.WriteAsync(buffer.ToArray(0, buffer.Length).AsMemory(0, buffer.Length));
+        }
+        catch (OperationCanceledException)
+        {
+        }
+        catch (Exception ex)
+        {
+            GD.PrintErr($"[{nameof(Connection)}] Error sending: {ex}");
+            Disconnect();
+        }
+    }
+
+    private async Task HandlePacketAsync(ByteBuffer buffer)
+    {
+        var verifier = new Verifier(buffer);
+        if (!verifier.VerifyPacketBase())
+        {
+            GD.PrintErr($"[{nameof(Connection)}] Invalid packet base");
+            Disconnect();
+            return;
+        }
+    }
 }
---
+++ Schema/compile.py
@@ -0,0 +1,103 @@
+import argparse
+import os
+import pathlib
+import re
+import subprocess
+
+FLATC_URL_LINUX = "https://github.com/google/flatbuffers/releases/download/v24.3.25/Linux.flatc.binary.g++-13.zip"
+FLATC_URL_WINDOWS = "https://github.com/google/flatbuffers/releases/download/v24.3.25/Windows.flatc.binary.zip"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--output", required=True)
+parser.add_argument("-i", "--input", required=True)
+parser.add_argument("--namespace", type=str, default="tower.net.packet")
+parser.add_argument("--lang", type=str, default="cpp")
+parser.add_argument("--filename-pascalcase", action="store_true")
+parser.add_argument("--download-flatc", action="store_true")
+parser.add_argument("--platform", type=str, default="linux")
+# parser.add_argument("files", metavar='N', nargs='+', type=str)
+args = parser.parse_args()
+
+
+# Configure templates
+files = pathlib.Path(args.input).rglob("*.fbs")
+for input_file in files:
+    path, filename = os.path.split(input_file)
+
+    filename = filename.replace(".template", "")
+    if args.filename_pascalcase:
+        filename = ''.join(word.capitalize() for word in filename.split('_'))
+    
+    output_path = os.path.join("out", path)
+    output_file = os.path.join("out", path, filename)
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    with open(input_file, 'r') as src, open(output_file, 'w') as dst:
+        lines = src.readlines()
+
+        # Change include file path pascal case
+        if args.filename_pascalcase:
+            for i, line in enumerate(lines):
+                m = re.match(r'^include "([^"]*)";', line)
+                if not m:
+                    continue
+
+                path, filename = os.path.split(m.group(1))
+                filename = ''.join(word.capitalize() for word in filename.split('_'))
+
+                lines[i] = f'include "{os.path.join(path, filename)}";\n'
+
+        content = "".join(lines)
+        content = content.replace("${packet_namespace}", args.namespace)
+        dst.write(content)
+    
+    print(f"{input_file} -> {output_file}")
+
+
+# Download flatc
+if args.download_flatc:
+    import stat
+    from io import BytesIO
+    from urllib.request import urlopen
+    from zipfile import ZipFile
+
+    if args.platform == "linux":
+        url = FLATC_URL_LINUX
+    elif args.platform == "windows":
+        url = FLATC_URL_WINDOWS
+
+    print(f"Downloading and extracting flatc from {url}...")
+    resp = urlopen(url)
+    file = ZipFile(BytesIO(resp.read()))
+    file.extract("flatc")
+
+    # chmod +x
+    st = os.stat("flatc")
+    os.chmod("flatc", st.st_mode | stat.S_IEXEC )
+
+
+# Compile schemas
+schemas = pathlib.Path("out").rglob("*.fbs")
+
+flatc_args = ["./flatc"]
+if args.lang == "cpp":
+    pass
+elif args.lang == "csharp":
+    flatc_args += ["--csharp", "--filename-suffix", '""', "-o", args.output, "-I", f"{os.path.join("out", args.input)}"]
+    flatc_args += [schema.as_posix() for schema in schemas]
+
+print(flatc_args)
+subprocess.run(flatc_args)
+
+# Move out generated files from excessive namespace-path
+if args.lang == "csharp":
+    import shutil
+
+    for file in pathlib.Path(os.path.join(args.output, *args.namespace.split("."))).glob("*"):
+        target_file = os.path.join(args.output, os.path.basename(file))
+        if os.path.exists(target_file):
+            os.remove(target_file)
+
+        shutil.move(file, args.output)
